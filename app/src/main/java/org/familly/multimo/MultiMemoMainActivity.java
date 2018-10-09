ac95be67938c4a78bfe69e542d6c59dbc15d6411
sinaburokim8725 <sinaburokim8725@gmail.com>
2018-10-09 16:07:56 +0900

    Initial commit 단계단계 나갑니다.

---
+++ app/src/main/java/org/familly/multimo/MultiMemoMainActivity.java
@@ -0,0 +1,13 @@
+package org.familly.multimo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MultiMemoMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multi_memo_main);
+    }
+}
